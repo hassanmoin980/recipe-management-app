b5c30be9c7d2be685d0f0a2e79adc0179f6042eb
hassanmoin980 <hassan.moin@gmail.com>
2023-08-29 19:42:10 +0500

    created reources folder

--- 03-basic-build/resources/recipe.py
+++ 03-basic-build/resources/recipe.py
@@ -25,3 +25,58 @@ class RecipeListResource(Resource):
         )
         recipe_list.append(recipe)
         return recipe.data, HTTPStatus.CREATED
+
+
+class RecipeResource(Resource):
+    def get(self, recipe_id):
+        recipe = next(
+            (
+                recipe
+                for recipe in recipe_list
+                if recipe.id == recipe_id and recipe.is_publish == True
+            ),
+            None,
+        )
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+
+    def put(self, recipe_id):
+        data = request.get_json()
+
+        recipe = next(
+            (recipe for recipe in recipe_list if recipe.id == recipe_id), None
+        )
+
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+
+        recipe.name = data["name"]
+        recipe.description = data["description"]
+        recipe.num_of_servings = data["num_of_servings"]
+        recipe.cook_time = data["cook_time"]
+        recipe.directions = data["directions"]
+        return recipe.data, HTTPStatus.OK
+
+
+class RecipePublishResource(Resource):
+    def put(self, recipe_id):
+        recipe = next(
+            (recipe for recipe in recipe_list if recipe.id == recipe_id), None
+        )
+
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = True
+        return {}, HTTPStatus.NO_CONTENT
+
+    def delete(self, recipe_id):
+        recipe = next(
+            (recipe for recipe in recipe_list if recipe.id == recipe_id), None
+        )
+
+        if recipe is None:
+            return {"message": "recipe not found"}, HTTPStatus.NOT_FOUND
+
+        recipe.is_publish = False
+        return {}, HTTPStatus.NO_CONTENT
